a066d12ab88d9695f3f8454c626fe210f6112b35
ehsan sayyadi <ehsansayadi7189@gamil.com>
2023-12-14 14:52:39 +0330

    initial an paginationClass to do paginate books and in this class craeted a method for show show page

---
+++ src/Commands/Regulateoperators/PaginationClass.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lenovo\Assignment\Commands\Regulateoperators;
+
+class PaginationClass
+{
+
+    public array$pagination_books;
+    public $books;
+    public $per_page=4;
+    public $page_show;
+
+    public function __construct($books)
+    {
+        $this->pagination_books= array_chunk($books,$this->per_page);
+    }
+    public  function pageShow($page_number)
+    {
+
+        $this->page_show = $this->pagination_books[$page_number];
+
+    }
+
+}
